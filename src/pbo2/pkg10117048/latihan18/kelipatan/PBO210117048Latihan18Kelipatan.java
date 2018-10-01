003af091c9bf5470e78fd5258d638bacdea018b7
Rizal <43487664+rizalarifnugraha@users.noreply.github.com>
2018-10-01 19:14:07 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan18/kelipatan/PBO210117048Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama              : Rizal Arif Nugraha 
+ * Kelas             : PBO2
+ * NIM               : 10117048 
+ * Deskripsi Program : Program ini menggunakan fungsi pengulangan while untuk
+ *                     mencari kelipatan pada bilangan.
+ */
+public class PBO210117048Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+         double i = 3.5;
+       
+       while(i <= 35) {
+           System.out.println(i);
+           
+           i = i + 3.5;
+       }
+    }
+
+}
